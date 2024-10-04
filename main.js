d4d6616699289448552d36fe9fce03bb84b2d5a9
iliketoridemybikebro <iliketoridemybikebro124@gmail.com>
2024-10-04 16:37:33 -0700

    Added link to csgoskins and also added rarity based coloring to the skin name

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
+skinLink = document.getElementById("skinLink")
+
 async function main() {
     const items = await getApi();
 
     const randomItem = items[Math.floor(Math.random() * items.length)]
     console.log(randomItem)
+    console.log(randomItem.market_hash_name)
+    console.log(randomItem.name)
+    console.log(encodeURI(`https://csgoskins.gg/?query=${randomItem.market_hash_name}`))
 
-    document.getElementById("skinImage").src = randomItem.image
+    //document.getElementById("skinImage").src = randomItem.image
+    skinLink.href = encodeURI("https://csgoskins.gg/?query=" + randomItem.name) + "%20(Factory%20New)"
+    skinLink.textContent = randomItem.name
+    skinLink.style.color = randomItem.rarity.color
+
 }
 
 async function getApi() {
-    response = await fetch('https://bymykel.github.io/CSGO-API/api/en/skins_not_grouped.json', {
+    response = await fetch('https://bymykel.github.io/CSGO-API/api/en/skins.json', {
       method: 'GET',
       headers: {
           'Accept': 'application/json',
